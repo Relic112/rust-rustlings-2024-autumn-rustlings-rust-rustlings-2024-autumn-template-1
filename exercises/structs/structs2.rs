59d7fc09b5a9dd71b13555e5834aeb19da24a351
Relilc112 <1033220132@stu.jiangnan.edu.cn>
2024-10-01 00:03:05 +0800

    32

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -1,12 +1,3 @@
-// structs2.rs
-//
-// Address all the TODOs to make the tests pass!
-//
-// Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -37,8 +28,9 @@ mod tests {
     #[test]
     fn your_order() {
         let order_template = create_order_template();
-        // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        // 使用更新语法创建新的 Order 实例
+        let your_order = Order { ..order_template };
+
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
